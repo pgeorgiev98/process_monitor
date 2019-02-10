4ce0ef989f928d04bf87ff3bd4493c5e000cd63c
Petko Georgiev <petko.vas.georgiev@gmail.com>
2019-02-11 00:08:18 +0200

    Change cells background to red when a process has a higher disk usage

--- src/interface.rs
+++ src/interface.rs
@@ -23,6 +23,7 @@ struct ProcessView {
     model: ListStore,
     tree: TreeView,
     processes_in_model: Vec<(u64, TreeIter)>,
+    default_background_color: (f64, f64, f64),
 }
 
 impl Interface {
@@ -78,13 +79,13 @@ impl Interface {
 
 impl ProcessView {
     fn new() -> ProcessView {
-        let model = ListStore::new(&[u64::static_type(), String::static_type(), String::static_type(), String::static_type()]);
+        let model = ListStore::new(&[u64::static_type(), String::static_type(), String::static_type(), String::static_type(), String::static_type(), String::static_type()]);
         let tree = TreeView::new();
 
-        append_column(&tree, 0, "PID");
-        append_column(&tree, 1, "Process Name");
-        append_column(&tree, 2, "Read Bytes");
-        append_column(&tree, 3, "Write Bytes");
+        append_column(&tree, 0, "PID", None);
+        append_column(&tree, 1, "Process Name", None);
+        append_column(&tree, 2, "Read Bytes", Some(4));
+        append_column(&tree, 3, "Write Bytes", Some(5));
 
         tree.set_model(Some(&model));
 
@@ -92,11 +93,20 @@ impl ProcessView {
             column.set_sort_column_id(i as i32);
         }
 
+        let default_background_color = match tree.get_style_context() {
+            Some(context) => {
+                let col = context.get_background_color(gtk::StateFlags::ACTIVE);
+                (col.red, col.green, col.blue)
+            },
+            None => (1.0, 1.0, 1.0),
+        };
+
         ProcessView {
             processes: ProcessesList::new(),
             model: model,
             tree: tree,
             processes_in_model: Vec::new(),
+            default_background_color,
         }
     }
 
@@ -125,34 +135,80 @@ impl ProcessView {
     }
 
     fn set_process(&self, iter: &TreeIter, process: &Process) {
-        let (read_bytes, write_bytes) = match &process.io_stats {
-            Ok(s) => (format_bytes_per_second(s.read_bytes), format_bytes_per_second(s.write_bytes)),
-            Err(_) => (String::from("-"), String::from("-")),
+        let (read_bytes, write_bytes, read_coef, write_coef) = match &process.io_stats {
+            Ok(s) => (
+                format_bytes_per_second(s.read_bytes),
+                format_bytes_per_second(s.write_bytes),
+                if self.processes.disk_stats.maximum_read > 0 {
+                    (s.read_bytes as f64) / (self.processes.disk_stats.maximum_read as f64)
+                } else {
+                    0.0
+                },
+                if self.processes.disk_stats.maximum_write > 0 {
+                    (s.write_bytes as f64) / (self.processes.disk_stats.maximum_write as f64)
+                } else {
+                    0.0
+                }
+            ),
+            Err(_) => (String::from("-"), String::from("-"), 0.0, 0.0),
         };
         let default_name = String::from("?");
         let name = match &process.name {
             Ok(n) => n,
             Err(_) => &default_name,
         };
-        self.model.set(&iter, &[0, 1, 2, 3], &[&process.pid, &name, &read_bytes, &write_bytes]);
+
+        let bgr = format!("#{:02X}{:02X}{:02X}",
+                          (read_coef * 255.0 + (1.0 - read_coef) * self.default_background_color.0 * 255.0) as i32,
+                          (read_coef *  17.0 + (1.0 - read_coef) * self.default_background_color.1 * 255.0) as i32,
+                          (read_coef *  51.0 + (1.0 - read_coef) * self.default_background_color.2 * 255.0) as i32);
+        let bgw = format!("#{:02X}{:02X}{:02X}",
+                          (write_coef * 255.0 + (1.0 - write_coef) * self.default_background_color.0 * 255.0) as i32,
+                          (write_coef *  17.0 + (1.0 - write_coef) * self.default_background_color.1 * 255.0) as i32,
+                          (write_coef *  51.0 + (1.0 - write_coef) * self.default_background_color.2 * 255.0) as i32);
+
+        self.model.set(&iter, &[0, 1, 2, 3, 4, 5], &[&process.pid, &name, &read_bytes, &write_bytes, &bgr, &bgw]);
     }
 
     fn append_process(&self, process: &Process) -> TreeIter {
-        let (read_bytes, write_bytes) = match &process.io_stats {
-            Ok(s) => (format_bytes_per_second(s.read_bytes), format_bytes_per_second(s.write_bytes)),
-            Err(_) => (String::from("-"), String::from("-")),
+        let (read_bytes, write_bytes, read_coef, write_coef) = match &process.io_stats {
+            Ok(s) => (
+                format_bytes_per_second(s.read_bytes),
+                format_bytes_per_second(s.write_bytes),
+                if self.processes.disk_stats.maximum_read > 0 {
+                    (s.read_bytes as f64) / (self.processes.disk_stats.maximum_read as f64)
+                } else {
+                    0.0
+                },
+                if self.processes.disk_stats.maximum_write > 0 {
+                    (s.write_bytes as f64) / (self.processes.disk_stats.maximum_write as f64)
+                } else {
+                    0.0
+                }
+            ),
+            Err(_) => (String::from("-"), String::from("-"), 0.0, 0.0),
         };
         let default_name = String::from("?");
         let name = match &process.name {
             Ok(n) => n,
             Err(_) => &default_name,
         };
-        self.model.insert_with_values(None, &[0, 1, 2, 3], &[&process.pid, &name, &read_bytes, &write_bytes])
+
+        let bgr = format!("#{:02X}{:02X}{:02X}",
+                          (read_coef * 255.0 + (1.0 - read_coef) * self.default_background_color.0 * 255.0) as i32,
+                          (read_coef *  17.0 + (1.0 - read_coef) * self.default_background_color.1 * 255.0) as i32,
+                          (read_coef *  51.0 + (1.0 - read_coef) * self.default_background_color.2 * 255.0) as i32);
+        let bgw = format!("#{:02X}{:02X}{:02X}",
+                          (write_coef * 255.0 + (1.0 - write_coef) * self.default_background_color.0 * 255.0) as i32,
+                          (write_coef *  17.0 + (1.0 - write_coef) * self.default_background_color.1 * 255.0) as i32,
+                          (write_coef *  51.0 + (1.0 - write_coef) * self.default_background_color.2 * 255.0) as i32);
+
+        self.model.insert_with_values(None, &[0, 1, 2, 3, 4, 5], &[&process.pid, &name, &read_bytes, &write_bytes, &bgr, &bgw])
     }
 }
 
 
-fn append_column(tree: &TreeView, id: i32, title: &'static str) {
+fn append_column(tree: &TreeView, id: i32, title: &'static str, background_id: Option<i32>) {
     let column = TreeViewColumn::new();
     let cell = CellRendererText::new();
 
@@ -161,6 +217,9 @@ fn append_column(tree: &TreeView, id: i32, title: &'static str) {
     column.pack_start(&cell, true);
     // Association of the view's column with the model's `id` column.
     column.add_attribute(&cell, "text", id);
+    if let Some(background_id) = background_id {
+        column.add_attribute(&cell, "background", background_id);
+    }
     tree.append_column(&column);
 }
 
